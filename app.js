858fbcb0c3538ba01702aeef8f4e4e5018448172
sathwiq <sathwiq301198@gmail.com>
2019-08-05 23:38:47 +0530

    a

--- app.js
+++ app.js
@@ -4,11 +4,37 @@ var express = require('express'),
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./views/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://homemade-de936.firebaseio.com"
+});
+let db = admin.firestore();
 var vendorRoutes   = require("./routes/vendor");
 app.use("/vendor", vendorRoutes);
 app.get('/', function (req, res) {
+    db.collection('menu').get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+        // db.collection('doc.id.menu.id').get()
+        //     .then((snapshot) => {
+        //         snapshot.forEach((doc) => {
+                    
+        //         console.log(doc.id);
+        //         })
+    //   console.log(doc.id);
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
+  
  res.render("index.ejs");
 });
 app.listen(process.env.PORT || 3001, function () {
  console.log('Example app listening on port !');
-});
+})
--- routes/vendor.js
+++ routes/vendor.js
@@ -1,13 +1,35 @@
 var express = require('express'),
      router = express.Router(); 
+var admin = require("firebase-admin");
 
+// var serviceAccount = require("../views/serviceAccountKey.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://homemade-de936.firebaseio.com"
+// });
+let db = admin.firestore();
 router.get('/', (req, res) => {
     res.render("vendor.ejs");
 });
 router.post("/",  function(req, res){
     // get data from form and add to campgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var desc = req.body.description;
+
+    let data = {
+        name: req.body.name,
+        address: req.body.address,
+        mobile: req.body.mobile,
+        email : req.body.email,
+        image : req.body.image,
+        
+        menu :  [
+            {
+                name : "coke",
+                price : '12',
+                image : 'url'
+            }
+        ]
+      };
+      db.collection('vendor').doc('NyqJsmPJ9Z15TVbhX8bt').set(data);
 });
 module.exports = router;
